1996f5249bd50c5e2d3e4a4cfa122b5f2822dff5
Kalyan Chavali <kalyan.chavali123@gmail.com>
2024-03-07 20:51:33 -0800

    init commit

---
+++ src/main/java/com/example/letEmCook/LetThemCookApplication.java
@@ -0,0 +1,13 @@
+package com.example.letEmCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetThemCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetThemCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/letEmCook/LetThemCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letEmCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetThemCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
